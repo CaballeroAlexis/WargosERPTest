8c28e1ec3f90b0f8e8d4900252a18903511a9e5e
Alexis <alexis.wolfy@gmail.com>
2018-08-14 16:11:08 -0300

    Estructura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarSideComponent } from './components/navbar-side/navbar-side.component';
+import { ClientComponent } from './components/client/client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarSideComponent,
+    ClientComponent
   ],
   imports: [
     BrowserModule
