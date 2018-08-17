671a7f50c47e87df7fb18356636e91f52d108b90
Alexis <alexis.wolfy@gmail.com>
2018-08-17 17:02:53 -0300

    Primer update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { APP_ROUTING } from './app.routes';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NavbarSideComponent } from './components/navbar-side/navbar-side.component';
 import { ClientComponent } from './components/client/client.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalClientComponent } from './components/client/modal-client/modal-client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     NavbarSideComponent,
-    ClientComponent
+    ClientComponent,
+    ModalClientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
   styleUrls: ['./client.component.css']
 })
 export class ClientComponent implements OnInit {
-
-  constructor() { }
+  closeResult: string;
+  constructor(private router:Router, private modalService: NgbModal) { }
 
   ngOnInit() {
   }
+  myFunction(){
+    var input, filter, table, tr, td, i;
+          input = document.getElementById("myInput");
+          filter = input.value.toUpperCase();
+          table = document.getElementById("tableClient");
+          tr = table.getElementsByTagName("tr");
+          // Loop through all table rows, and hide those who don't match the search query
+          for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+              if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+              } else {
+                tr[i].style.display = "none";
+              }
+            } 
+          }
+  }
 
+  open(content:any) {
+    
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {this.closeResult = `Closed with: ${result}`;
+}, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+    console.log("OPEN")
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      console.log("ESC");
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+      console.log("Click");
+    } else {
+      return  `with: ${reason}`;
+      console.log("MEHG");
+    }
+  }   
 }
